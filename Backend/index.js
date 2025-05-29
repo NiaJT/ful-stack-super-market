1f0f5ad523c9531f91c6ef809a360c6a8b992cd1
UDBO <nirajthapa457@gmail.com>
2025-05-29 21:51:24 +0545

    added rate limiter

--- Backend/index.js
+++ Backend/index.js
@@ -4,8 +4,19 @@ import { userController } from "./user/user.controller.js";
 import { productController } from "./product/product.controller.js";
 import { cartController } from "./cart/cart.controller.js";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  limit: 10, // Limit each IP to 10 requests per `window` (here, per 1 minute).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
 const app = express();
 const PORT = 8000;
+
+app.use(limiter);
 app.use(
   cors({
     origin: ["http://localhost:3000", "http://192.168.1.9:3000/"],
